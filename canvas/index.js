44d6d99eb8001cd75182b3dc2a0511adacad3f1b
Razor <therazor113@gmail.com>
2022-07-08 11:13:53 -0400

    more random

--- canvas/index.js
+++ canvas/index.js
@@ -1,10 +1,31 @@
-const canvas = document.querySelector('.canvas')
-const obj = canvas.getContext("2d")
+document.querySelector('.push').addEventListener('click', () => {
+  for (let i=1;i<=10;i++) {
+    new cube(
+    Math.random()*260+15,
+    Math.random()*100+15,
+    Math.random()*7+1,
+    Math.floor(Math.random()*16777215).toString(16)).show()
+  }
+})
+let x = document.createElement('canvas')
+x.style.backgroundColor = 'black'
+let obj = x.getContext('2d')
 
-const colorFade = obj.createLinearGradient(0, 0, 200, 0)
-colorFade.addColorStop(0, "purple")
-colorFade.addColorStop(1, "white")
 
-obj.fillStyle = colorFade
-obj.font = "30px arial"
-obj.fillText('Hello, World!', 10, 100)
+document.body.appendChild(x)
+
+class cube {
+  constructor(x, y, width, color) {
+    this.x = x
+    this.y = y
+    this.color = color
+    this.width = width
+  }
+  show() {
+    obj.beginPath()
+    obj.lineWidth = this.width
+    obj.arc(this.x, this.y, 10, 0, 2 * Math.PI);
+    obj.strokeStyle = `#${this.color}`
+    obj.stroke()
+  }
+}
